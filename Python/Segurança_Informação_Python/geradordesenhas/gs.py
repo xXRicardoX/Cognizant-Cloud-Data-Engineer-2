9acecb86359c2a04a6cb62a36430300fbcc915c2
xXRicardoX <ricardo.ferreiracarvalho@hotmail.com>
2022-04-20 00:43:52 -0300

    Criando um gerador de senha

---
+++ Python/Segurança_Informação_Python/geradordesenhas/gs.py
@@ -0,0 +1,9 @@
+import random, string
+
+tamanho = int(input('Digite o tamanho de senha que você deseja: '))
+
+chars = string.ascii_letters + string.digits + '!@#$%&*()-=+,.;:/?'
+
+rnd = random.SystemRandom() #os.urandom
+
+print(''.join(rnd.choice(chars) for i in range(tamanho)))
